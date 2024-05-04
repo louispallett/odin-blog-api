487e28c510ccf678966469f9a12be261d7f5ce8d
louispallett <louis.pallett@outlook.com>
2024-05-04 11:16:24 +0100

    Export models

--- server/models/comment.js
+++ server/models/comment.js
@@ -1,10 +1,17 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const comment = new Schema(
+const Comment = new Schema(
     {
         author: { type: Schema.Types.ObjectId, ref: "User", required: true },
         content: { type: String, required: true },
         date: { type: Date, required: true },
+        likes: { type: Number, required: true },
     }
 )
+
+Comment.virtual("date_formatted").get(function() {
+    return DateTime.fromJSDate(this.date).toLocaleString(DateTime.DATETIME_MED);
+});
+
+module.exports = mongoose.model("Comment", Comment)
--- server/models/post.js
+++ server/models/post.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const post = new Schema(
+const Post = new Schema(
     {
         author: { type: Schema.Types.ObjectId, ref: "User", required: true },
         title: { type: String, required: true },
@@ -9,4 +9,6 @@ const post = new Schema(
         image_url: { type: String },
         date: { type: Date, required: true },
     }
-)
+);
+
+module.exports = mongoose.model("Post", Post);
