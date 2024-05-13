74a2440e035049ba37b4501ddcd212b7b3eb978c
louispallett <louis.pallett@outlook.com>
2024-05-13 20:28:43 +0100

    Add synopsis to model

--- server/models/article.js
+++ server/models/article.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const Post = new Schema(
+const Article = new Schema(
     {
         author: { type: Schema.Types.ObjectId, ref: "User", required: true },
         title: { type: String, required: true },
+        synopsis: { type: String, required: true },
         content: { type: String, required: true },
-        image_url: { type: String },
+        image_url: { type: String }, // If this isn't provided, we can put in a 'placeholder' image
         date: { type: Date, required: true },
     }
 );
 
-module.exports = mongoose.model("Article", Post);
+module.exports = mongoose.model("Article", Article);
