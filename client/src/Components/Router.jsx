be4204455958558ac272ebd72ff6749144a8d59d
Louis Pallett <louis.pallett@outlook.com>
2024-05-16 23:30:17 +0100

    Add outstanding tasks

--- client/src/Components/Router.jsx
+++ client/src/Components/Router.jsx
@@ -1,3 +1,20 @@
+/* 
+========================
+OUTSTANDING TASKS
+========================
+
+* Authorization * 
+
+    We've got to CHECK for authorization on the front end, checking whether the "authorzation" token is there and if it
+    is valid, then we can display different things to the user.
+
+* Edit Site *
+
+    Create a seperate front end (a whole seperate front end is probably the easiest for this, so that we can have a different
+    authorization token etc.) where users can create articles etc.
+
+*/
+
 import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
 
 import Dashboard from "./Dashboard";
