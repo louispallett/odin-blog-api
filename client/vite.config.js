12b912b3d89fd4802bffba141742a34bd7779333
Louis Pallett <louis.pallett@outlook.com>
2024-06-03 14:49:56 +0100

    Update server site

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://son-server.fly.dev/',
         changeOrigin: true,
         secure: false,
         ws: true,
