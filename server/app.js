2955b22aebd2b140eb4c32dfc6e0575d055c2258
Louis Pallett <louis.pallett@outlook.com>
2024-06-04 16:50:07 +0100

    Update headers and redirects file

--- server/app.js
+++ server/app.js
@@ -36,7 +36,7 @@ const app = express();
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "POST, GET");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
     next();
 });
 
